4bf7990464784249ccb4a39f648b4871771d7b57
Armando <pyarmando.academia@gmail.com>
2026-07-13 21:34:05 -0500

    Bucles

---
+++ bucles.py
@@ -0,0 +1,36 @@
+"""
+break continue
+- Modifican el comportamiento de los bucles for y while 
+"""
+
+#for x in range(1,11):
+#    print(f"Hola llegue hasta {x}")
+#    if x==5:
+#        break #romper o detener el bucle
+#print("FIN DEL PROGRAMA")
+    
+
+"""
+contador = 1
+while contador <=10:
+    print(contador)
+    if contador ==5:
+        break
+    contador +=1
+print("Programa Terminado")    
+
+
+for numero in range(1,11):
+    if numero ==5:
+        continue
+    print(numero)    
+    
+"""
+
+contador =0
+while contador <10:
+    contador += 1
+    if contador ==5:
+        continue
+    print(contador)
+    
